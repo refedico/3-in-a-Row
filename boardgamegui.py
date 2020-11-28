506879cae0b6c4a19bb9b3546989f9f51912a9ed
refedico <47032913+refedico@users.noreply.github.com>
2020-11-28 16:30:59 +0100

    Update boardgamegui.py

--- boardgamegui.py
+++ boardgamegui.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python3
-'''
-@author  Michele Tomaiuolo - http://www.ce.unipr.it/people/tomamic
-@license This software is free - http://www.gnu.org/licenses/gpl.html
-'''
-
 import g2d
 from boardgame import BoardGame
 from time import time
