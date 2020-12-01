0db6e4abbe37ed037f052c58a565d4733d071f7d
refedico <47032913+refedico@users.noreply.github.com>
2020-12-01 02:20:51 +0100

    Update boardgame.py

--- boardgame.py
+++ boardgame.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python3
-'''
-@author  Michele Tomaiuolo - http://www.ce.unipr.it/people/tomamic
-@license This software is free - http://www.gnu.org/licenses/gpl.html
-'''
-
 def abstract():
     raise NotImplementedError("Abstract method")
 
@@ -11,6 +5,7 @@ class BoardGame:
     def play_at(self, x: int, y: int): abstract()
     def flag_at(self, x: int, y: int): abstract()
     def value_at(self, x: int, y: int) -> str: abstract()
+    def automatism(self): abstract()
     def cols(self) -> int: abstract()
     def rows(self) -> int: abstract()
     def finished(self) -> bool: abstract()
